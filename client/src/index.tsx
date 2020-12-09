ba80cd794eb67aebe81c6453f53a119a096cd3a2
smarttin <smarttin77@gmail.com>
2020-12-09 23:49:37 +0100

    init nextBnB :( a home sharing platform

---
+++ client/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Happy!!</div>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
