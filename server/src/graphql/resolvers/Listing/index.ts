535af39b088f4eeeacbbd72d05b0be90c3b84bda
smarttin <smarttin77@gmail.com>
2020-12-14 08:21:08 +0100

    gecode location with google geocode api

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,6 +1,7 @@
 import {IResolvers} from 'apollo-server-express';
 import {Request} from 'express';
 import {ObjectId} from 'mongodb';
+import {Google} from '../../../lib/api';
 import {Database, Listing, User} from '../../../lib/types';
 import {authorize} from '../../../lib/utils';
 import {
@@ -10,6 +11,7 @@ import {
   ListingsArgs,
   ListingsData,
   ListingsFilter,
+  ListingsQuery,
 } from './types';
 
 export const listingResolvers: IResolvers = {
@@ -37,16 +39,34 @@ export const listingResolvers: IResolvers = {
     },
     listings: async (
       _root: undefined,
-      {filter, limit, page}: ListingsArgs,
+      {location, filter, limit, page}: ListingsArgs,
       {db}: {db: Database},
     ): Promise<ListingsData> => {
       try {
+        const query: ListingsQuery = {};
         const data: ListingsData = {
+          region: null,
           total: 0,
           result: [],
         };
 
-        let cursor = await db.listings.find({});
+        if (location) {
+          const {country, admin, city} = await Google.geocode(location);
+
+          if (city) query.city = city;
+          if (admin) query.admin = admin;
+          if (country) {
+            query.country = country;
+          } else {
+            throw new Error('no country found');
+          }
+
+          const cityText = city ? `${city}, ` : "";
+          const adminText = admin ? `${admin}, ` : "";
+          data.region = `${cityText}${adminText}${country}`;
+        }
+
+        let cursor = await db.listings.find(query);
 
         if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
           cursor = cursor.sort({price: 1});
--- server/src/lib/api/Google.ts
+++ server/src/lib/api/Google.ts
@@ -1,3 +1,4 @@
+import {createClient} from '@google/maps';
 import {google} from 'googleapis';
 
 const auth = new google.auth.OAuth2(
@@ -5,6 +6,30 @@ const auth = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_SECRET,
   `${process.env.PUBLIC_URL}/login`,
 );
+
+const maps = createClient({key: `${process.env.GOOGLE_GEOCODE_KEY}`, Promise});
+
+const parseAddress = (addressComponents: any[]) => {
+  let country = null;
+  let admin = null;
+  let city = null;
+
+  for (const component of addressComponents) {
+    if (component.types.includes('country')) {
+      country = component.long_name;
+    }
+
+    if (component.types.includes('administrative_area_level_1')) {
+      admin = component.long_name;
+    }
+
+    if (component.types.includes('locality') || component.types.includes('postal_town')) {
+      city = component.long_name;
+    }
+  }
+
+  return {country, admin, city};
+};
 
 export const Google = {
   authUrl: auth.generateAuthUrl({
@@ -25,5 +50,14 @@ export const Google = {
     });
 
     return {user: data};
+  },
+  geocode: async (address: string) => {
+    const res = await maps.geocode({address}).asPromise();
+
+    if (res.status < 200 || res.status > 299) {
+      throw new Error('Failed to geocode address');
+    }
+
+    return parseAddress(res.json.results[0].address_components);
   },
 };
